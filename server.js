aeeb319cfc13f559a63aa90ddab6832a84729b54
practice-logs <aniketjambhulkar1692@gmail.com>
2026-07-05 13:50:57 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+// Test route
+app.get("/", (req, res) => {
+    res.json({
+        status: "ok",
+        message: "Railway WebRTC Signaling Server Running"
+    });
+});
+
+// Socket connection
+io.on("connection", (socket) => {
+
+    console.log("Client connected:", socket.id);
+
+    socket.on("offer", (data) => {
+        socket.broadcast.emit("offer", data);
+    });
+
+    socket.on("answer", (data) => {
+        socket.broadcast.emit("answer", data);
+    });
+
+    socket.on("ice", (data) => {
+        socket.broadcast.emit("ice", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected:", socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
